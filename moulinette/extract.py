21dc6a195fff7ad3264c6d43ff9bcf0d8e29275a
Emmanuel Jovanovic <cavinomanu@gmail.com>
2024-03-05 14:54:01 +0100

    moulinette ok (date generateur) todo: post + tests

--- moulinette/extract.py
+++ moulinette/extract.py
@@ -1,29 +1,26 @@
+from datetime import datetime
 import requests
 import gzip
 import pandas as pd
 import os
 from io import BytesIO
-from datetime import datetime
 
-def generate_monthly_dates(start: str, end: str) -> list:
+def generate_monthly_dates_generator(start: str, end: str):
     start_date = datetime.strptime(start, "%Y%m")
     end_date = datetime.strptime(end, "%Y%m")
     
-    dates = []
     while start_date <= end_date:
-        dates.append(start_date.strftime("%Y%m"))
+        yield start_date.strftime("%Y%m")
         if start_date.month == 12:
             start_date = datetime(start_date.year + 1, 1, 1)
         else:
             start_date = datetime(start_date.year, start_date.month + 1, 1)
-    
-    return dates
 
 json_folder = "./json"
 base_url = "https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop.{date}.csv.gz"
-monthly_dates = generate_monthly_dates("199601", datetime.now().strftime("%Y%m"))
+monthly_dates_generator = generate_monthly_dates_generator("199601", datetime.now().strftime("%Y%m"))
 
-for date in monthly_dates:
+for date in monthly_dates_generator:
     url = base_url.format(date=date)
     json_filename = f"synop.{date}.json"
     
