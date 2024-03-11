69687d4ec4486a39eb521d14d912863deafe2987
lgrandmorcel <leo.grandmorcel@ynov.com>
2024-03-11 11:56:27 +0100

    add get and post

--- app/main.py
+++ app/main.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
+from typing import List, Dict
+
+import os
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from app.model import Meteo
+
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root() -> dict[str, str]:
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="templates")
+
+
+def get_all_charts() -> List[Dict[str, str]]:
+    charts = []
+    for file in os.listdir("static/charts/"):
+        if file.endswith(".gif") or file.endswith(".png") or file.endswith(".jpg"):
+            filename = file.split(".")[0]
+            file_info = {"name": filename, "href": file}
+            charts.append(file_info)
+    return charts
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request) -> HTMLResponse:
+    charts = get_all_charts()
+    return templates.TemplateResponse(
+        name="index.html", request=request, context={"charts": charts}
+    )
+
+
+@app.post("/upload/")
+async def post(meteo: List[Meteo]) -> List[Meteo]:
+    return meteo
+
+
+@app.get("/chart/", response_class=HTMLResponse)
+async def chart(
+    request: Request,
+    chart_name: str = "temperature",
+) -> HTMLResponse:
+    print(chart_name)
+    return templates.TemplateResponse(
+        name="chart.html", request=request, context={"chart": "test.gif"}
+    )
