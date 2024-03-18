c24c99c73d0c3c058c311517ed1e0c72c6274795
lgrandmorcel <leo.grandmorcel@ynov.com>
2024-03-18 09:13:36 +0100

    Modification

---
+++ moulinette/upload.py
@@ -0,0 +1,23 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from moulinette.extract import monthly_dates_generator, request_data, process_data
+
+POST_URL = "http://localhost:8000/upload/"
+
+
+def upload_data(data: dict) -> requests.Response:
+    headers = {"Content-Type": "application/json"}
+    return requests.post(POST_URL, data=data, headers=headers, timeout=15)
+
+
+def combinatoire(monthly_date: str) -> requests.Response:
+    return upload_data(process_data(request_data(monthly_date)))
+
+
+def main() -> None:
+    with ThreadPoolExecutor(max_workers=16) as executor:
+        executor.map(combinatoire, monthly_dates_generator())
+
+
+if __name__ == "__main__":
+    main()
