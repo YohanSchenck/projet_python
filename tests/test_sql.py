d09166338ad2dc7eb8afbeaafbf50827ae62f619
syohan <yohan.schenck@ynov.com>
2024-03-18 10:26:37 +0100

    clarification des tests ; test evolution_diff

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,10 +1,15 @@
 import os
-from datetime import datetime
 from typing import List
 
 import pytest
 from app.model import Meteo
-from app.sql import create_db, get_evolution_temp, insert_data
+from app.sql import (
+    create_db,
+    get_evolution_diff_temperature,
+    get_evolution_temp,
+    get_evolution_wind,
+    insert_data,
+)
 from sqlmodel import Session, SQLModel, create_engine, select
 
 
@@ -15,22 +20,53 @@ def init_database():
     return engine
 
 
-def test_insert_data(init_database) -> None:
+@pytest.fixture
+def create_2_Meteo():
     data: List[Meteo] = []
-    date = datetime.now()
-
-    engine = init_database
 
     meteo1 = Meteo(
         station_id=102,
-        year=date.year,
-        month=date.month,
-        day=date.day,
-        hour=date.hour,
+        year=2024,
+        month=1,
+        week=1,
+        day=1,
+        hour=9,
         wind=20.5,
         temperature=10.3,
     )
+
+    meteo2 = Meteo(
+        station_id=102,
+        year=2024,
+        month=1,
+        week=1,
+        day=2,
+        hour=9,
+        wind=20.5,
+        temperature=20.3,
+    )
+
+    meteo3 = Meteo(
+        station_id=102,
+        year=2023,
+        month=1,
+        week=1,
+        day=1,
+        hour=9,
+        wind=20.5,
+        temperature=10.3,
+    )
+
     data.append(meteo1)
+    data.append(meteo2)
+    data.append(meteo3)
+    return data
+
+
+def test_insert_data(init_database, create_2_Meteo) -> None:
+    data = create_2_Meteo
+
+    engine = init_database
 
     insert_data(engine, data)
 
@@ -38,9 +74,9 @@ def test_insert_data(init_database) -> None:
         statement = select(Meteo)
         result = session.exec(statement).all()
         assert (result[0].station_id) == 102
-        assert (result[0].year) == date.year
-        assert (result[0].month) == date.month
-        assert (result[0].day) == date.day
+        assert (result[0].year) == 2024
+        assert (result[0].month) == 1
+        assert (result[0].day) == 1
         assert (result[0].wind) == 20.5
         assert (result[0].temperature) == 10.3
 
@@ -50,38 +86,34 @@ def test_create_database() -> None:
     assert (os.path.isfile("database.db")) is True
 
 
-def test_get_evolution_temp(init_database) -> None:
-    data: List[Meteo] = []
-    date = datetime.now()
-
+def test_get_evolution_temp(init_database, create_2_Meteo) -> None:
+    data = create_2_Meteo
     engine = init_database
-
-    meteo1 = Meteo(
-        station_id=102,
-        year=date.year,
-        month=date.month,
-        day=date.day,
-        hour=date.hour,
-        wind=20.5,
-        temperature=10.3,
-    )
-
-    meteo2 = Meteo(
-        station_id=102,
-        year=date.year - 1,
-        month=date.month,
-        day=date.day,
-        hour=date.hour,
-        wind=20.5,
-        temperature=10.3,
-    )
-
-    data.append(meteo1)
-    data.append(meteo2)
-
     insert_data(engine, data)
 
     df = get_evolution_temp(engine)
 
     assert (len(df)) == 2
     assert (len(df.columns)) == 3
+
+
+def test_get_evolution_wind(init_database, create_2_Meteo) -> None:
+    data = create_2_Meteo
+    engine = init_database
+    insert_data(engine, data)
+
+    df = get_evolution_wind(engine)
+
+    assert (len(df)) == 3
+    assert (len(df.columns)) == 4
+
+
+def test_get_evolution_diff_temperature(init_database, create_2_Meteo) -> None:
+    data = create_2_Meteo
+    engine = init_database
+    insert_data(engine, data)
+
+    df = get_evolution_diff_temperature(engine)
+
+    assert (len(df)) == 2
+    assert (len(df.columns)) == 4
